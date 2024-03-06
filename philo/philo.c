7ffaec385be647abbe6c07e2a4fd5a298a3290e4
Pedro <pedro@energia50.ciemat.es>
2024-03-06 11:16:28 +0100

    fix philo eat n times

--- philo/philo.c
+++ philo/philo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   philo.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pedro <pedro@student.42.fr>                +#+  +:+       +#+        */
+/*   By: pedromar <pedromar@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 21:16:43 by pedromar          #+#    #+#             */
-/*   Updated: 2024/03/05 22:48:54 by pedro            ###   ########.fr       */
+/*   Updated: 2024/03/06 11:15:38 by pedromar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,12 +26,12 @@ static void	philo_wait(t_local *philo, t_time wait)
 static void	philo_think(t_local *philo, t_share *sh)
 {
 	printer(philo, MSG_THINK, THINK);
-	while (sh->states[philo->fork_one] != 0 && philo->share->dies == 0)
+	while (sh->states[philo->fork_one] != 0 && sh->dies == 0)
 		philo_wait(philo, 10);
 	pthread_mutex_lock(&sh->forks[philo->fork_one]);
 	memset(&sh->states[philo->fork_one], 1, sizeof(char));
 	printer(philo, MSG_TAKE_FORK, FORK);
-	while (sh->states[philo->fork_two] != 0 && philo->share->dies == 0)
+	while (sh->states[philo->fork_two] != 0 && sh->dies == 0)
 		philo_wait(philo, 10);
 	pthread_mutex_lock(&sh->forks[philo->fork_two]);
 	memset(&sh->states[philo->fork_two], 1, sizeof(char));
@@ -48,16 +48,17 @@ static void	philo_sleep(t_local *philo, t_share *sh, t_global *gl)
 	philo_wait(philo, gl->times[SLEEP]);
 }
 
-static void	philo_eat(t_local *philo, t_share *sh, t_global *gl)
+static void	philo_eat(t_local *philo)
 {
 	static int	n_eats[PTHREAD_THREAD_MAX];
 
-	philo->time_to_die = gl->times[DIE];
 	printer(philo, MSG_EAT, EAT);
-	philo_wait(philo, gl->times[EAT]);
-	if (n_eats[philo->id -1]++ == gl->n_eats)
-		memset(&(sh->complete), sh->complete +1,
+	philo->time_to_die = philo->global->times[DIE];
+	if (n_eats[philo->id -1] == philo->global->n_eats)
+		memset(&(philo->share->complete), philo->share->complete +1,
 			sizeof(int));
+	n_eats[philo->id -1]++;
+	philo_wait(philo, philo->global->times[EAT]);
 }
 
 void	*philosopher(void *arg)
@@ -78,10 +79,10 @@ void	*philosopher(void *arg)
 	while (philo->share->start == 0)
 		;
 	while (philo->share->dies == 0
-		&& philo->share->complete < philo->global->n_philo)
+		&& philo->share->complete != philo->global->n_philo)
 	{
 		philo_think(philo, philo->share);
-		philo_eat(philo, philo->share, philo->global);
+		philo_eat(philo);
 		philo_sleep(philo, philo->share, philo->global);
 	}
 	return (NULL);
